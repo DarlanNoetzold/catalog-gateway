f40cd846bf4d8f5237ebf0568eeeeb260228eaab
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-10-17 07:46:28 -0300

    Update ProductModel.java

--- src/main/java/tech/noetzold/model/ProductModel.java
+++ src/main/java/tech/noetzold/model/ProductModel.java
@@ -1,4 +1,24 @@
 package tech.noetzold.model;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class ProductModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID productId;
+
 }
