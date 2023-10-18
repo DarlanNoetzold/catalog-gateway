d9ba1d18e8a3347acbfa2d7747bf7653dec457bf
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-10-18 15:35:23 -0300

    Update ProductModel.java

--- src/main/java/tech/noetzold/model/ProductModel.java
+++ src/main/java/tech/noetzold/model/ProductModel.java
@@ -1,14 +1,15 @@
 package tech.noetzold.model;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
+import org.wildfly.common.annotation.NotNull;
 
+import java.util.List;
 import java.util.UUID;
 
 @Data
@@ -20,5 +21,52 @@ public class ProductModel {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID productId;
+
+    private String title;
+
+    private String description;
+
+    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "categoryId")
+    private CategoryModel category;
+
+    private String brand;
+
+    private String model;
+
+    private String videoUrl;
+
+    private String gender;
+
+    private Integer warrantyTime;
+
+    private String warrantyText;
+
+    private Number height;
+
+    private Number width;
+
+    private Number weight;
+
+    private Number length;
+
+    private Number priceFactor;
+
+    private Boolean calculatedPrice;
+
+    private String definitionPriceScope;
+
+    private Boolean hasVariations;
+
+    @OneToMany(mappedBy = "product")
+    @Fetch(FetchMode.JOIN)
+    private List<SkuModel> skus;
+
+    private Boolean allowAutomaticSkuMarketplaceCreation;
+
+    private String code;
+
+    private String message;
 
 }
