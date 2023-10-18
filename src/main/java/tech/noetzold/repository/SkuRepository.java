e905160774ae38e586f27215d51eae1a1f2e0d93
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-10-18 18:49:43 -0300

    Update SkuRepository.java

--- src/main/java/tech/noetzold/repository/SkuRepository.java
+++ src/main/java/tech/noetzold/repository/SkuRepository.java
@@ -3,8 +3,20 @@ package tech.noetzold.repository;
 
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
 import jakarta.enterprise.context.ApplicationScoped;
+import tech.noetzold.model.ProductModel;
 import tech.noetzold.model.SkuModel;
+
+import java.util.Optional;
+import java.util.UUID;
 
 @ApplicationScoped
 public class SkuRepository implements PanacheRepository<SkuModel> {
+
+    public Optional<SkuModel> findByIdOptional(UUID id) {
+        return find("skuId", id).firstResultOptional();
+    }
+
+    public void deleteById(UUID id) {
+        delete("skuId", id);
+    }
 }
