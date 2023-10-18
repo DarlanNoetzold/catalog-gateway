a0908c6f4c1a38b5510bf7b2878989a0304f0555
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-10-18 15:35:16 -0300

    Create Dimension.java

---
+++ src/main/java/tech/noetzold/model/Dimension.java
@@ -0,0 +1,13 @@
+package tech.noetzold.model;
+
+import java.math.BigDecimal;
+
+public class Dimension {
+
+    private BigDecimal width;
+    private String lwhUom;
+    private BigDecimal height;
+    private BigDecimal length;
+    private BigDecimal weight;
+    private String weightUom;
+}
