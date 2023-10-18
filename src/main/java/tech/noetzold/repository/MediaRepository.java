78c20101af4dc0c2aebfee65608cde7701ebceaf
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-10-18 18:49:36 -0300

    Update MediaRepository.java

--- src/main/java/tech/noetzold/repository/MediaRepository.java
+++ src/main/java/tech/noetzold/repository/MediaRepository.java
@@ -2,8 +2,20 @@ package tech.noetzold.repository;
 
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
 import jakarta.enterprise.context.ApplicationScoped;
+import tech.noetzold.model.KeyWordModel;
 import tech.noetzold.model.MediaModel;
+
+import java.util.Optional;
+import java.util.UUID;
 
 @ApplicationScoped
 public class MediaRepository implements PanacheRepository<MediaModel> {
+
+    public Optional<MediaModel> findByIdOptional(UUID id) {
+        return find("mediaId", id).firstResultOptional();
+    }
+
+    public void deleteById(UUID id) {
+        delete("mediaId", id);
+    }
 }
