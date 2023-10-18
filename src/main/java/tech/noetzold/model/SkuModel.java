30fb5f9f1d2da9e4a81112a5fef464d05e3f8f48
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-10-18 15:35:25 -0300

    Update SkuModel.java

--- src/main/java/tech/noetzold/model/SkuModel.java
+++ src/main/java/tech/noetzold/model/SkuModel.java
@@ -1,13 +1,15 @@
 package tech.noetzold.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
+import org.wildfly.common.annotation.NotNull;
 
+import java.math.BigDecimal;
+import java.util.List;
 import java.util.UUID;
 
 @Data
@@ -19,4 +21,38 @@ public class SkuModel {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID skuId;
+
+    private Long id;
+
+    private String displayName;
+
+    private String partnerId;
+
+    private Long sellerId;
+
+    private String ean;
+
+    private BigDecimal price;
+
+    private BigDecimal salePrice;
+
+    private Long stockLevel;
+
+    private Boolean enabled;
+
+    private Dimension packageDimensionModel;
+
+    private Dimension contentDimensionModel;
+
+    private List<AttributeModel> attributes;
+
+    @OneToMany(mappedBy = "sku")
+    @Fetch(FetchMode.JOIN)
+    private List<MediaModel> medias;
+
+    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "productId")
+    private ProductModel product;
+
 }
