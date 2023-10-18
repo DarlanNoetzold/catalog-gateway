da7eedf7bd5069d366c73cdcdc73749c4c36119e
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-10-18 15:35:11 -0300

    Create AttributeModel.java

---
+++ src/main/java/tech/noetzold/model/AttributeModel.java
@@ -0,0 +1,37 @@
+package tech.noetzold.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class AttributeModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID attributeId;
+
+    private String name;
+
+    private String description;
+
+    private String type;
+
+    private String imageUrl;
+
+    private String hexCode;
+
+    private String internalName;
+
+    private String priority;
+
+}
