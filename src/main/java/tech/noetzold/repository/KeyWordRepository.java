f6d52f29f6b76bfe3c47d9dd82b394b629113e06
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-10-18 18:49:34 -0300

    Update KeyWordRepository.java

--- src/main/java/tech/noetzold/repository/KeyWordRepository.java
+++ src/main/java/tech/noetzold/repository/KeyWordRepository.java
@@ -2,8 +2,20 @@ package tech.noetzold.repository;
 
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
 import jakarta.enterprise.context.ApplicationScoped;
+import tech.noetzold.model.CategoryModel;
 import tech.noetzold.model.KeyWordModel;
+
+import java.util.Optional;
+import java.util.UUID;
 
 @ApplicationScoped
 public class KeyWordRepository implements PanacheRepository<KeyWordModel> {
+
+    public Optional<KeyWordModel> findByIdOptional(UUID id) {
+        return find("keyWordId", id).firstResultOptional();
+    }
+
+    public void deleteById(UUID id) {
+        delete("keyWordId", id);
+    }
 }
