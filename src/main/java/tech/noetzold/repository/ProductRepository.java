5f5d78a1dfa9c6f11ec1269102a53d1bc4586759
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-10-19 20:03:44 -0300

    Create ProductRepository.java

---
+++ src/main/java/tech/noetzold/repository/ProductRepository.java
@@ -0,0 +1,19 @@
+package tech.noetzold.repository;
+
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+import jakarta.enterprise.context.ApplicationScoped;
+import tech.noetzold.model.ProductModel;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@ApplicationScoped
+public class ProductRepository implements PanacheRepository<ProductModel> {
+    public Optional<ProductModel> findByIdOptional(UUID id) {
+        return find("productId", id).firstResultOptional();
+    }
+
+    public void deleteById(UUID id) {
+        delete("productId", id);
+    }
+}
