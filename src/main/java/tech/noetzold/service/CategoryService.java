4c322d389631ef93f7fb5eb802bdeaeb174b523b
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-10-19 07:39:13 -0300

    Update CategoryService.java

--- src/main/java/tech/noetzold/service/CategoryService.java
+++ src/main/java/tech/noetzold/service/CategoryService.java
@@ -1,7 +1,60 @@
 package tech.noetzold.service;
 
+import io.quarkus.cache.CacheInvalidateAll;
+import io.quarkus.cache.CacheResult;
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.transaction.Transactional;
+import jakarta.ws.rs.WebApplicationException;
+import jakarta.ws.rs.core.Response;
+import tech.noetzold.model.CategoryModel;
+import tech.noetzold.repository.CategoryRepository;
+
+import java.util.Optional;
+import java.util.UUID;
 
 @ApplicationScoped
 public class CategoryService {
+    
+    @Inject
+    CategoryRepository categoryRepository;
+
+    @Transactional
+    @CacheResult(cacheName = "category")
+    public CategoryModel findCategoryModelById(UUID id){
+        Optional<CategoryModel> optionalCategoryModel = categoryRepository.findByIdOptional(id);
+        return optionalCategoryModel.orElse(new CategoryModel());
+    }
+
+    @Transactional
+    @CacheInvalidateAll(cacheName = "category")
+    public CategoryModel saveCategoryModel(CategoryModel categoryModel){
+        categoryRepository.persist(categoryModel);
+        categoryRepository.flush();
+        return categoryModel;
+    }
+
+    @Transactional
+    @CacheInvalidateAll(cacheName = "category")
+    public void updateCategoryModel(CategoryModel categoryModel){
+        if (categoryModel == null || categoryModel.getCategoryId() == null) {
+            throw new WebApplicationException("Invalid data for categoryModel update", Response.Status.BAD_REQUEST);
+        }
+
+        CategoryModel existingCategoryModel = findCategoryModelById(categoryModel.getCategoryId());
+        if (existingCategoryModel == null) {
+            throw new WebApplicationException("categoryModel not found", Response.Status.NOT_FOUND);
+        }
+
+        existingCategoryModel.setName(categoryModel.getName());
+        existingCategoryModel.setProducts(categoryModel.getProducts());
+
+        categoryRepository.persist(existingCategoryModel);
+    }
+
+    @Transactional
+    @CacheInvalidateAll(cacheName = "category")
+    public void deleteCategoryModelById(UUID id){
+        categoryRepository.deleteById(id);
+    }
 }
