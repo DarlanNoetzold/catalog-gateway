5ba35ba21dbea980b494712351a95bc348a62045
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-10-20 13:21:07 -0300

    Update AttributeController.java

--- src/main/java/tech/noetzold/controller/AttributeController.java
+++ src/main/java/tech/noetzold/controller/AttributeController.java
@@ -1,12 +1,18 @@
 package tech.noetzold.controller;
 
+import jakarta.inject.Inject;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import tech.noetzold.service.AttributeService;
 
 @Path("/api/catalog/v1/attribute")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public class AttributeController {
+
+    @Inject
+    AttributeService attributeService;
+
 }
