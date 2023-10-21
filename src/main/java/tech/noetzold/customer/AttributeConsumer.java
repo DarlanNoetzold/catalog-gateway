841a78cdbfc15499555869fada08aca7002bcd0f
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-10-21 07:49:20 -0300

    Update AttributeConsumer.java

--- src/main/java/tech/noetzold/customer/AttributeConsumer.java
+++ src/main/java/tech/noetzold/customer/AttributeConsumer.java
@@ -1,11 +1,15 @@
 package tech.noetzold.customer;
 
+import io.smallrye.reactive.messaging.annotations.Blocking;
+import io.smallrye.reactive.messaging.annotations.Merge;
+import io.vertx.core.json.JsonObject;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
+import org.eclipse.microprofile.reactive.messaging.Incoming;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import tech.noetzold.model.AttributeModel;
 import tech.noetzold.service.AttributeService;
-import tech.noetzold.service.KeyWordService;
 
 @ApplicationScoped
 public class AttributeConsumer {
@@ -14,4 +18,17 @@ public class AttributeConsumer {
     AttributeService attributeService;
 
     private static final Logger logger = LoggerFactory.getLogger(AttributeConsumer.class);
+
+    @Incoming("attributes")
+    @Merge
+    @Blocking
+    public AttributeModel process(JsonObject incomingAttributeModelInJson) {
+
+        AttributeModel incomingAttributeModel = incomingAttributeModelInJson.mapTo(AttributeModel.class);
+
+        attributeService.saveAttributeModel(incomingAttributeModel);
+        logger.info("Create Attribute " + incomingAttributeModel.getAttributeId() + ".");
+
+        return incomingAttributeModel;
+    }
 }
