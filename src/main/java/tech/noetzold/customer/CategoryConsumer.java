d9278e55d0810068aad531e022cd8792963a15d5
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-10-21 07:49:22 -0300

    Update CategoryConsumer.java

--- src/main/java/tech/noetzold/customer/CategoryConsumer.java
+++ src/main/java/tech/noetzold/customer/CategoryConsumer.java
@@ -1,9 +1,14 @@
 package tech.noetzold.customer;
 
+import io.smallrye.reactive.messaging.annotations.Blocking;
+import io.smallrye.reactive.messaging.annotations.Merge;
+import io.vertx.core.json.JsonObject;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
+import org.eclipse.microprofile.reactive.messaging.Incoming;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import tech.noetzold.model.CategoryModel;
 import tech.noetzold.service.CategoryService;
 
 @ApplicationScoped
@@ -13,4 +18,17 @@ public class CategoryConsumer {
     CategoryService categoryService;
 
     private static final Logger logger = LoggerFactory.getLogger(CategoryConsumer.class);
+
+    @Incoming("categories")
+    @Merge
+    @Blocking
+    public CategoryModel process(JsonObject incomingCategoryModelInJson) {
+
+        CategoryModel incomingCategoryModel = incomingCategoryModelInJson.mapTo(CategoryModel.class);
+
+        categoryService.saveCategoryModel(incomingCategoryModel);
+        logger.info("Create Category " + incomingCategoryModel.getCategoryId() + ".");
+
+        return incomingCategoryModel;
+    }
 }
