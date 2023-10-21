d1aacfbb23aabe836eaf1c2136cc52ce62d8164e
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-10-21 07:49:29 -0300

    Update ProductConsumer.java

--- src/main/java/tech/noetzold/customer/ProductConsumer.java
+++ src/main/java/tech/noetzold/customer/ProductConsumer.java
@@ -1,9 +1,14 @@
 package tech.noetzold.customer;
 
+import io.smallrye.reactive.messaging.annotations.Blocking;
+import io.smallrye.reactive.messaging.annotations.Merge;
+import io.vertx.core.json.JsonObject;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
+import org.eclipse.microprofile.reactive.messaging.Incoming;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import tech.noetzold.model.ProductModel;
 import tech.noetzold.service.ProductService;
 
 @ApplicationScoped
@@ -13,4 +18,17 @@ public class ProductConsumer {
     ProductService productService;
 
     private static final Logger logger = LoggerFactory.getLogger(ProductConsumer.class);
+
+    @Incoming("products")
+    @Merge
+    @Blocking
+    public ProductModel process(JsonObject incomingProductModelInJson) {
+
+        ProductModel incomingProductModel = incomingProductModelInJson.mapTo(ProductModel.class);
+
+        productService.saveProductModel(incomingProductModel);
+        logger.info("Create Product " + incomingProductModel.getProductId() + ".");
+
+        return incomingProductModel;
+    }
 }
