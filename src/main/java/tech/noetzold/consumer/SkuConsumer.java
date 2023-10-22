b426ee50f74fcc2a60d92712d074c0b4be8de1ce
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-10-22 10:18:50 -0300

    Create SkuConsumer.java

---
+++ src/main/java/tech/noetzold/consumer/SkuConsumer.java
@@ -0,0 +1,32 @@
+package tech.noetzold.consumer;
+
+import io.smallrye.reactive.messaging.annotations.Blocking;
+import io.smallrye.reactive.messaging.annotations.Merge;
+import io.vertx.core.json.JsonObject;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import org.eclipse.microprofile.reactive.messaging.Incoming;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import tech.noetzold.model.SkuModel;
+import tech.noetzold.service.SkuService;
+
+@ApplicationScoped
+public class SkuConsumer {
+
+    @Inject
+    SkuService skuService;
+
+    private static final Logger logger = LoggerFactory.getLogger(SkuConsumer.class);
+
+    @Incoming("skus")
+    @Merge
+    @Blocking
+    public SkuModel process(SkuModel incomingSkuModel) {
+
+        skuService.saveSkuModel(incomingSkuModel);
+        logger.info("Create Sku " + incomingSkuModel.getSkuId() + ".");
+
+        return incomingSkuModel;
+    }
+}
