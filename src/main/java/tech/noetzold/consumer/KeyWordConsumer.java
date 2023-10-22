347c87261b74844e9bb56847597a42548765ab2c
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-10-22 10:18:42 -0300

    Create KeyWordConsumer.java

---
+++ src/main/java/tech/noetzold/consumer/KeyWordConsumer.java
@@ -0,0 +1,32 @@
+package tech.noetzold.consumer;
+
+import io.smallrye.reactive.messaging.annotations.Blocking;
+import io.smallrye.reactive.messaging.annotations.Merge;
+import io.vertx.core.json.JsonObject;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import org.eclipse.microprofile.reactive.messaging.Incoming;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import tech.noetzold.model.KeyWordModel;
+import tech.noetzold.service.KeyWordService;
+
+@ApplicationScoped
+public class KeyWordConsumer {
+
+    @Inject
+    KeyWordService keyWordService;
+
+    private static final Logger logger = LoggerFactory.getLogger(KeyWordConsumer.class);
+
+    @Incoming("keywords")
+    @Merge
+    @Blocking
+    public KeyWordModel process(KeyWordModel incomingKeyWordModel) {
+
+        keyWordService.saveKeyWordModel(incomingKeyWordModel);
+        logger.info("Create KeyWord " + incomingKeyWordModel.getKeyWordId() + ".");
+
+        return incomingKeyWordModel;
+    }
+}
