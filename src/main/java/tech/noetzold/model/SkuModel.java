e1ca18dabe84d41577a5f7e9f4bfb9709a93e3d9
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-10-27 19:25:53 -0300

    Update SkuModel.java

--- src/main/java/tech/noetzold/model/SkuModel.java
+++ src/main/java/tech/noetzold/model/SkuModel.java
@@ -4,8 +4,6 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.Fetch;
-import org.hibernate.annotations.FetchMode;
 import org.wildfly.common.annotation.NotNull;
 
 import java.math.BigDecimal;
