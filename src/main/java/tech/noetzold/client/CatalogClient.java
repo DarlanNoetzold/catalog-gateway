d38fb4b3244b75129adf466105f0ecb2c660c410
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-11-30 07:49:59 -0300

    Update CatalogClient.java

--- src/main/java/tech/noetzold/client/CatalogClient.java
+++ src/main/java/tech/noetzold/client/CatalogClient.java
@@ -3,14 +3,34 @@ package tech.noetzold.client;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 
-import tech.noetzold.model.SkuModel;
+import tech.noetzold.model.*;
 
-@Path("/ecommerce/v1/auth")
+@Path("http://localhost:8091/ecommerce/v1/catalog/")
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
 public interface CatalogClient {
 
     @POST
-    @Path("/save")
-    SkuModel saveSku(@HeaderParam("Authorization") String token, SkuModel skuModel);
+    @Path("/sku")
+    SkuModel sendSku(@HeaderParam("Authorization") String token, SkuModel skuModel);
+
+    @POST
+    @Path("/product")
+    SkuModel sendProduct(@HeaderParam("Authorization") String token, ProductModel productModel);
+
+    @POST
+    @Path("/media")
+    SkuModel sendMedia(@HeaderParam("Authorization") String token, MediaModel mediaModel);
+
+    @POST
+    @Path("/keyword")
+    SkuModel sendKeyWord(@HeaderParam("Authorization") String token, KeyWordModel keyWordModel);
+
+    @POST
+    @Path("/category")
+    SkuModel sendCategory(@HeaderParam("Authorization") String token, CategoryModel categoryModel);
+
+    @POST
+    @Path("/attribute")
+    SkuModel sendAttribute(@HeaderParam("Authorization") String token, AttributeModel attributeModel);
 }
