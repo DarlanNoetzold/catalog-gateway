90b9b36f9db67fd365f1fa3fc3a0a9f99895f397
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-11-30 08:10:24 -0300

    Update ProductConsumer.java

--- src/main/java/tech/noetzold/consumer/ProductConsumer.java
+++ src/main/java/tech/noetzold/consumer/ProductConsumer.java
@@ -8,6 +8,7 @@ import org.eclipse.microprofile.reactive.messaging.Incoming;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import tech.noetzold.model.ProductModel;
+import tech.noetzold.service.CatalogService;
 import tech.noetzold.service.ProductService;
 
 @ApplicationScoped
@@ -16,13 +17,16 @@ public class ProductConsumer {
     @Inject
     ProductService productService;
 
+    @Inject
+    CatalogService catalogService;
+
     private static final Logger logger = LoggerFactory.getLogger(ProductConsumer.class);
 
     @Incoming("products")
     @Merge
     @Blocking
     public ProductModel process(ProductModel incomingProductModel) {
-
+        catalogService.sendProduct(incomingProductModel);
         productService.saveProductModel(incomingProductModel);
         logger.info("Create Product " + incomingProductModel.getProductId() + ".");
 
