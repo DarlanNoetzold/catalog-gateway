a062aa15e1ff996a0762fcecfe355640dcb9173e
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-11-30 08:10:19 -0300

    Update AttributeConsumer.java

--- src/main/java/tech/noetzold/consumer/AttributeConsumer.java
+++ src/main/java/tech/noetzold/consumer/AttributeConsumer.java
@@ -9,6 +9,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import tech.noetzold.model.AttributeModel;
 import tech.noetzold.service.AttributeService;
+import tech.noetzold.service.CatalogService;
 
 @ApplicationScoped
 public class AttributeConsumer {
@@ -16,13 +17,16 @@ public class AttributeConsumer {
     @Inject
     AttributeService attributeService;
 
+    @Inject
+    CatalogService catalogService;
+
     private static final Logger logger = LoggerFactory.getLogger(AttributeConsumer.class);
 
     @Incoming("attributes")
     @Merge
     @Blocking
     public AttributeModel process(AttributeModel incomingAttributeModel) {
-
+        catalogService.sendAttribute(incomingAttributeModel);
         attributeService.saveAttributeModel(incomingAttributeModel);
         logger.info("Create Attribute " + incomingAttributeModel.getAttributeId() + ".");
 
