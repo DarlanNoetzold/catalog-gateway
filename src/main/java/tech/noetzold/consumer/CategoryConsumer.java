a5591ad1647d13dfac1126e0f12efa550f793ab7
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-11-30 08:10:33 -0300

    Update CategoryConsumer.java

--- src/main/java/tech/noetzold/consumer/CategoryConsumer.java
+++ src/main/java/tech/noetzold/consumer/CategoryConsumer.java
@@ -8,6 +8,7 @@ import org.eclipse.microprofile.reactive.messaging.Incoming;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import tech.noetzold.model.CategoryModel;
+import tech.noetzold.service.CatalogService;
 import tech.noetzold.service.CategoryService;
 
 @ApplicationScoped
@@ -16,13 +17,16 @@ public class CategoryConsumer {
     @Inject
     CategoryService categoryService;
 
+    @Inject
+    CatalogService catalogService;
+
     private static final Logger logger = LoggerFactory.getLogger(CategoryConsumer.class);
 
     @Incoming("categories")
     @Merge
     @Blocking
     public CategoryModel process(CategoryModel incomingCategoryModel) {
-
+        catalogService.sendCategory(incomingCategoryModel);
         categoryService.saveCategoryModel(incomingCategoryModel);
         logger.info("Create Category " + incomingCategoryModel.getCategoryId() + ".");
 
