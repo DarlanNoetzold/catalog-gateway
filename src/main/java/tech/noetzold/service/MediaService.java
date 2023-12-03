1896d6a81fd72b005ee8c91892e88c9d4c60541d
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-12-03 10:43:37 -0300

    Update MediaService.java

--- src/main/java/tech/noetzold/service/MediaService.java
+++ src/main/java/tech/noetzold/service/MediaService.java
@@ -2,14 +2,17 @@ package tech.noetzold.service;
 
 import io.quarkus.cache.CacheInvalidateAll;
 import io.quarkus.cache.CacheResult;
+import io.quarkus.hibernate.orm.panache.PanacheQuery;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import jakarta.transaction.Transactional;
 import jakarta.ws.rs.WebApplicationException;
 import jakarta.ws.rs.core.Response;
 import tech.noetzold.model.MediaModel;
+import tech.noetzold.model.MediaModel;
 import tech.noetzold.repository.MediaRepository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -32,6 +35,12 @@ public class MediaService {
         mediaRepository.persist(mediaModel);
         mediaRepository.flush();
         return mediaModel;
+    }
+
+    @Transactional
+    public List<MediaModel> findAllMediaModel(){
+        PanacheQuery<MediaModel> allMediaModel = mediaRepository.findAll();
+        return allMediaModel.list();
     }
 
     @Transactional
